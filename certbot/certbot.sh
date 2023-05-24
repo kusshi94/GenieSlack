185b8f777dc386740fdb6b0360b4f826adaa194d
root <51787281+kusshi94@users.noreply.github.com>
2023-05-24 19:55:17 +0900

    fix: nginx Dockerfile to watch certificate

--- certbot/certbot.sh
+++ certbot/certbot.sh
@@ -2,7 +2,7 @@
 
 trap exit TERM
 while :; do 
-    certbot renew -w /var/www/certbot --post-hook "nginx -s reload" --quiet
+    certbot renew -w /var/www/certbot --quiet
     sleep 7d &
     wait $!
 done
