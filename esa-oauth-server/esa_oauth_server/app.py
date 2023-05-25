b6e6a7c04ade3220ba6834e5ee63de67ebf7bd02
kawayama <kawa130307@gmail.com>
2023-05-25 09:18:22 +0900

    rebase: データベース接続以外のesa oauthの実装
    Poetry環境の作成
    
    add: ライブラリの追加
    
    add: Dockerfileの追加、docker-compose.ymlの修正
    
    add: Add api files
    
    fix: nginxの設定ファイルに設定を追加
    
    add: doenvファイルから読み込むように変更
    
    fix: Dockerfileの修正
    
    fix: flaskのサーバ設定を修正
    
    fix: nginxのパス設定を修正
    
    fix: Dockerfileの修正

---
+++ esa-oauth-server/esa_oauth_server/app.py
@@ -0,0 +1,98 @@
+import datetime
+import os
+
+import dotenv
+from flask import Flask, render_template, redirect, request, session
+from oauthlib.oauth2.rfc6749.errors import InvalidGrantError
+from requests_oauthlib import OAuth2Session
+
+dotenv.load_dotenv()
+client_id = os.environ['ESA_CLIENT_ID']
+client_secret = os.environ['ESA_CLIENT_SECRET']
+
+redirect_uri = 'https://genieslack.kusshi.dev/esa/redirect_uri'
+scope = ['read', 'write']
+session_min = 10
+
+app = Flask(__name__, template_folder='../templates')
+app.secret_key = os.urandom(24)
+app.permanent_session_lifetime = datetime.timedelta(minutes=session_min)
+
+
+# 認可開始エンドポイント /esa/oauth?team_id=<rand_value>
+@app.route('/esa/oauth')
+def start_oauth():
+    if 'rand_value' not in request.args:
+        return render_template(
+            'error.html', 
+            error_statement='Slackのワークスペース情報がありません。もう一度、正しいEsaの認可URLにアクセスして下さい。'
+        )
+
+    rand_value = request.args['rand_value']
+    # TODO: データベースからrand_valueを使ってteam_idを取得
+    slack_team_id = '<ThisIsATestValue!!!!!!!!!!!>'
+
+    session.parmanet = True
+    session['slack_team_id'] = slack_team_id
+    
+    oauth = OAuth2Session(
+        client_id=client_id,
+        redirect_uri=redirect_uri,
+        scope=scope,
+    )
+
+    authorization_url, state = oauth.authorization_url('https://api.esa.io/oauth/authorize')
+    print(authorization_url, state)
+
+    session['state'] = state
+
+    return redirect(authorization_url)
+
+
+# リダイレクトURI /esa/redirect_uri?code=<認可コード>&state=<Stateの値>
+@app.route('/esa/redirect_uri')
+def callback():
+    if 'state' not in session:
+        return render_template(
+            'error.html', 
+            error_statement=f"認可に{session_min}分以上かかり、セッションが切れています。再度SlackのURLからやり直してください。"
+        )
+    elif 'state' not in request.args:
+        return render_template(
+            'error.html', 
+            error_statement=f"state情報がありません。もう一度、正しいEsaの認可URLにアクセスして下さい。"
+        )
+    elif session['state'] != request.args['state']:
+        return render_template(
+            'error.html', 
+            error_statement=f"stateが一致しません。再度、アクセスしてください"
+        )
+
+    oauth = OAuth2Session(
+        client_id=client_id,
+        redirect_uri=redirect_uri,
+        scope=scope,
+    )
+
+    try:
+        token = oauth.fetch_token(
+            token_url='https://api.esa.io/oauth/token',
+            client_secret=client_secret,
+            code=request.args.get('code'),
+        )
+    except InvalidGrantError:
+        return render_template(
+            'error.html', 
+            error_statement=f"認可コードが正しくありません。再度、アクセスしてください"
+        )
+
+    # TODO: 後で消す
+    print(token['access_token'], session['slack_team_id'])
+
+    # TODO: データベース保存
+
+    return render_template('finish.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=5000)
