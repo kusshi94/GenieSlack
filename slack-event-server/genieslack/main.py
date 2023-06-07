233415f65a8fd5f6294dd0af73aad0c91601b525
MasaMizo <72693374+MasaMizo@users.noreply.github.com>
2023-06-08 01:02:58 +0900

    add to slack のurl修正

--- slack-event-server/genieslack/main.py
+++ slack-event-server/genieslack/main.py
@@ -305,7 +305,7 @@ class OAuthFlow2(OAuthFlow):
                 <p class="subtitle">Let's manage <br> your knowledge easily!</p>
                 <p class="appname">GenieSlack</p>
                 <p class="description">重要な情報の見逃しや情報の散在を防ぎ、<br>チーム全体のコラボレーションを強化できます。</p>
-                <a href="{{html.escape(url)}}"><img alt="Add to Slack" height="48" width="167" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+                <a href="{html.escape(url)}"><img alt="Add to Slack" height="48" width="167" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
             </div>
         </div>
     </div>
